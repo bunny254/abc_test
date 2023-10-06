42cf8718fd1f2440c921563b3ce42805be6056e4
bunny254 <kimaniwachira03@gmail.com>
2023-10-06 06:33:57 +0300

    Added layout, routing to the application using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,63 @@
-//import Banner from "./siteComponents/banner"
+//Importing app dependencies
+import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
+
+
+//Importing All App Components
+import Banner from "./siteComponents/banner"
 import Footer from "./siteComponents/footer";
 import NavBar from "./siteComponents/navBar";
-//import Shop from "./siteComponents/shop"
-//import Cart from "./siteComponents/cart"
-//import SingleProduct from "./siteComponents/singleProdDisplay"
-//import Checkout from "./siteComponents/checkout"
-//import Contact from "./siteComponents/contact"
+import Shop from "./siteComponents/shop"
+import Cart from "./siteComponents/cart"
+import SingleProduct from "./siteComponents/singleProdDisplay"
+import Checkout from "./siteComponents/checkout"
+import Contact from "./siteComponents/contact"
 import Home from "./siteComponents/home";
+
+
+//Creating App Layout
+const Layout=()=>{
+  return(
+  <div>
+    <NavBar/>
+    <Outlet/>
+    <Footer/>
+  </div>
+  )
+}
+
+//Creating router
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>,
+    children:[
+      {
+        path:"/",
+        element:<Home/>
+      },
+      {
+        path:"/shop",
+        element:<Shop/>
+      },
+      {
+        path:"/single-product.",
+        element:<SingleProduct/>
+      },
+      {
+        path:"/cart",
+        element:<Cart/>
+      },
+      {
+        path:"/contact-us",
+        element:<Contact/>
+      },
+      {
+        path:"/checkout",
+        element:<Checkout/>
+      }
+    ]
+  }
+])
 
 function App() {
  
@@ -14,9 +65,7 @@ function App() {
   return (
     <>
       <div>
-        <NavBar/>
-        <Home/>
-        <Footer/>
+        <RouterProvider router={router}/>
       </div>
       
     </>
